32958de1df4edb6fdb6c4dc537485739ed9854f9
YeonwooKoh <kohyeonwoo413@gmail.com>
2023-04-04 14:11:57 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+
+// const loginForm = document.querySelector("#login-form");
+
+// const loginInput = loginForm.querySelector("input");
+// const loginButton = loginForm.querySelector("button");
+
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+
+function onLoginSubmit(event){
+
+    event.preventDefault();
+    const username= loginInput.value;
+    console.log(username);
+   
+}
+
+loginForm.addEventListener("submit",onLoginSubmit);
+
+onLoginSubmit();
